d1df2c0de4613fc22d89218029c2b6d04cd3f16a
Robert Prehn <prehnra@gmail.com>
2020-07-03 17:08:16 -0500

    feat: Add fomantic ui

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+import 'fomantic-ui-less/semantic.less';
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -5,6 +5,8 @@ const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+const nodeModulesPath = path.resolve(__dirname, 'node_modules')
+
 module.exports = (env, options) => {
   const devMode = options.mode !== 'production';
 
@@ -26,6 +28,24 @@ module.exports = (env, options) => {
     devtool: devMode ? 'source-map' : undefined,
     module: {
       rules: [
+        // For images and fonts found in our scss files
+        {
+          test: /\.(jpg|jpeg|gif|png)$/,
+          use: [
+            'file-loader',
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                disable: devMode,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(woff2?|ttf|eot|svg)(\?[a-z0-9\=\.]+)?$/,
+          exclude: [nodeModulesPath],
+          loader: 'file-loader',
+        },
         {
           test: /\.js$/,
           exclude: /node_modules/,
@@ -40,12 +60,31 @@ module.exports = (env, options) => {
             'css-loader',
             'sass-loader',
           ],
-        }
+        },
+        {
+          test: /\.less$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {loader: 'css-loader', options: {sourceMap: true}},
+            {
+              loader: 'less-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
       ]
     },
     plugins: [
       new MiniCssExtractPlugin({ filename: '../css/app.css' }),
       new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
-    ]
+    ],
+    resolve: {
+      alias: {
+        "../../theme.config$": path.join(__dirname, "/semantic-ui/theme.config"),
+        "../semantic-ui/site": path.join(__dirname, "/semantic-ui/site")
+      }
+    },
   }
 };
