59cd93ef669bde7ccb95887c56dbb91085c2072f
Robert Prehn <prehnra@gmail.com>
2020-07-03 10:34:19 -0500

    chore: Add database config for CI

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :legendary, Legendary.Repo,
   username: "postgres",
   password: "postgres",
   database: "legendary_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: System.get_env("DATABASE_URL") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
