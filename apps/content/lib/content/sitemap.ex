3a7fe7eb4711b3fdf722f12fe5580ee79cc2241f
Robert Prehn <prehnra@gmail.com>
2020-10-09 12:18:47 -0500

    fix: Add sitemap job back

---
+++ apps/content/lib/content/sitemap.ex
@@ -0,0 +1,45 @@
+defmodule Content.Sitemaps do
+  @moduledoc """
+    This module generates sitemaps for the website and pings search engines as
+    appropriate.
+  """
+  alias Content.{Endpoint, Post, Posts, Repo,  Router.Helpers, Terms}
+  import Ecto.Query
+
+  use Sitemap,
+    host: "https://#{Application.get_env(:content, Endpoint)[:url][:host]}",
+    files_path: "tmp/sitemap/",
+    public_path: "",
+    adapter: Content.SitemapStorage
+
+  def generate do
+    create do
+      add "", priority: 0.5, changefreq: "hourly", expires: nil
+
+      posts =
+        Posts.post_scope
+        |> where([p], p.type not in ["nav_menu_item", "attachment"])
+        |> Repo.all()
+
+        for post <- posts do
+        add Helpers.posts_path(Endpoint, :show, post), priority: 0.5, changefreq: "hourly", expires: nil
+        page_count = Post.content_page_count(post)
+        if page_count > 1 do
+          (2..page_count)
+          |> Enum.each(fn page ->
+            add Helpers.paged_post_path(Endpoint, :show, post, page), priority: 0.5, changefreq: "hourly", expires: nil
+          end)
+        end
+      end
+
+      Terms.categories
+      |> Repo.all()
+      |> Enum.each(fn category ->
+        add Helpers.category_path(Endpoint, :index_posts, category.slug), priority: 0.5, changefreq: "hourly", expires: nil
+      end)
+    end
+
+    # notify search engines (currently Google and Bing) of the updated sitemap
+    if Mix.env() == :prod, do: ping()
+  end
+end
---
+++ apps/content/lib/content/sitemap_storage.ex
@@ -0,0 +1,69 @@
+defmodule Content.SitemapStorage do
+  @moduledoc """
+    This module serves as a storage adapter for the Sitemap package. It writes
+    the sitemap as an attachment post into the system, so that the CMS will
+    serve it up.
+  """
+  alias Content.{Endpoint, Post, Repo, Router.Helpers, User}
+  alias Ecto.Changeset
+  alias Sitemap.{Location}
+  import Ecto.Query
+
+  @behaviour Sitemap.Adapters.Behaviour
+
+  def write(name, data) do
+    path = Location.filename(name)
+    existing_post = Post |> from() |> where([name: ^path]) |> Repo.one()
+
+    gzip? = Regex.match?(~r/.gz$/, path)
+
+    mime = case gzip? do
+      true ->
+        "application/gzip"
+      false ->
+        "application/xml"
+    end
+
+    data_processed = data |> :zlib.gzip() |> Base.encode64
+
+    post_params = %{
+      date: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second),
+      date_gmt: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second),
+      content: data_processed,
+      title: path,
+      excerpt: "",
+      status: "publish",
+      comment_status: "closed",
+      ping_status: "closed",
+      password: "",
+      name: path,
+      to_ping: "",
+      pinged: "",
+      modified: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second),
+      modified_gmt: NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second),
+      content_filtered: "",
+      parent: 0,
+      guid: Helpers.posts_url(Endpoint, :show, path),
+      menu_order: 0,
+      type: "attachment",
+      mime_type: mime,
+      comment_count: 0,
+      sticky: 0,
+    }
+
+    case existing_post do
+      nil ->
+        %Post{}
+        |> Changeset.change(post_params)
+        |> (fn (post) ->
+          try do
+            post |> Repo.insert!()
+          rescue
+            Ecto.ConstraintError -> post |> Repo.update!()
+          end
+        end).()
+      post ->
+        post |> Changeset.change(post_params) |> Repo.update!()
+    end
+  end
+end
