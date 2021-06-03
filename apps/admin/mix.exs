9fa40ca392eeefc30b5f2516330cc023f8155bad
Robert Prehn <prehnra@gmail.com>
2021-06-03 19:49:36 +0000

    chore(release): 2.4.6 [skip ci]

--- apps/admin/mix.exs
+++ apps/admin/mix.exs
@@ -1,7 +1,7 @@
 defmodule Legendary.Admin.MixProject do
   use Mix.Project
 
-  @version "2.4.5"
+  @version "2.4.6"
 
   def project do
     [
--- apps/content/mix.exs
+++ apps/content/mix.exs
@@ -1,7 +1,7 @@
 defmodule Legendary.Content.MixProject do
   use Mix.Project
 
-  @version "2.4.5"
+  @version "2.4.6"
 
   def project do
     [
